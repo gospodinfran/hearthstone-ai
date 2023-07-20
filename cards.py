e7b39be0fe9d05459534fdf9475d451a0f78e3fb
Fran <kezicfran@gmail.com>
2023-07-20 10:25:04 +0200

    formatting, added hero power concept

--- cards.py
+++ cards.py
@@ -2,9 +2,9 @@ from main import Card, Minion, Player
 from typing import Union
 import random
 
-# TODO, add targeting: opponent, minions, etc. Currently cards thats should target go face.
-
 # Game mechanics
+
+
 def choose_one(effect_one, effect_two):
     print("1. Effect one.")
     print("2. Effect two.")
@@ -24,29 +24,31 @@ def choose_one(effect_one, effect_two):
     except ValueError:
         print("Choose a valid index.")
 
+
 def choose_target_enemy(player: Player, opponent: Player):
     print("0. Opponent")
     for i, minion in enumerate(opponent.board):
         print(i + 1, minion.name)
-    print(len(opponent.board) + 1, "Cancel")
 
-    # TODO: implement cancel
+    # TODO: don't actually need the ability to cancel
 
     valid = False
     while not valid:
         index = int(input())
         if index == 0:
             return opponent
-        if 0 < index <= len(opponent.board): 
+        if 0 < index <= len(opponent.board):
             valid = True
             return opponent.board[index - 1]
+
 
 def destroyed_check_enemy(player: Player, opponent: Player, target: Union[Player, Minion]):
     if isinstance(target, Minion):
         if target.health == 0:
             index = opponent.board.index(target)
             opponent.board.pop(index)
-            opponent.destroyed[target.name] = opponent.destroyed.get(target.name, 0) + 1
+            opponent.destroyed[target.name] = opponent.destroyed.get(
+                target.name, 0) + 1
 
 
 # Card effects
@@ -63,26 +65,33 @@ def bloodsail_corsair(player: Player, opponent: Player):
         if opponent.weapon_durability == 0:
             opponent.weapon = None
 
+
 def abusive_sergeant(player: Player, opponent: Player):
     pass
+
 
 def elven_archer(player: Player, opponent: Player):
     pass
 
+
 def hungry_crab(player, opponent):
     pass
 
+
 def voodoo_doctor(player, opponent):
     pass
+
 
 def acidic_swamp_ooze(player: Player, opponent: Player):
     opponent.weapon = None
     opponent.weapon_durability = 0
 
+
 def bloodsail_raider(player: Player, opponent: Player):
     pass
 
 # Druid
+
 
 def innervate(player: Player, opponent: Player):
     player.mana += 2
@@ -99,10 +108,13 @@ def claw(player: Player, opponent: Player):
     player.armor += 2
 
 # Choose one effects
+
+
 def nourish(player: Player, opponent: Player):
     def one():
         player.max_mana += 2
         player.mana = min(player.mana + 2, 10)
+
     def two():
         player.draw(3)
     if choose_one(one, two) == False:
@@ -156,63 +168,89 @@ def gladiators_longbow(player: Player, opponent: Player):
 
 
 # currently-implemented cards
-card_names = ["innervate", "moonfire", "healing_touch", "wildfire_growth", "starfire"]
+card_names = ["innervate", "moonfire",
+              "healing_touch", "wildfire_growth", "starfire"]
 
-innervate_card = Card(cost=0, effect=innervate, name="Innervate", description="Gain 2 Mana Crystals this turn only.")
+innervate_card = Card(cost=0, effect=innervate, name="Innervate",
+                      description="Gain 2 Mana Crystals this turn only.")
 
-moonfire_card = Card(cost=0, effect=moonfire, name="Moonfire", description="Deal 1 damage")
+moonfire_card = Card(cost=0, effect=moonfire,
+                     name="Moonfire", description="Deal 1 damage")
 
-claw_card = Card(cost=1, effect=claw, name="Claw", description="Give your hero 2 attack this turn. Gain 2 armor.")
+claw_card = Card(cost=1, effect=claw, name="Claw",
+                 description="Give your hero 2 attack this turn. Gain 2 armor.")
 
-nourish_card = Card(cost=5, effect=nourish, name="Nourish", description="Choose one - Gain 2 Mana Crystals; or Draw 3 cards.")
+nourish_card = Card(cost=5, effect=nourish, name="Nourish",
+                    description="Choose one - Gain 2 Mana Crystals; or Draw 3 cards.")
 
-healing_touch_card = Card(cost=3, effect=healing_touch, name="Healing Touch", description="Restore 8 Health.")
+healing_touch_card = Card(cost=3, effect=healing_touch,
+                          name="Healing Touch", description="Restore 8 Health.")
 
-wildfire_growth_card = Card(cost=2, effect=wildfire_growth, name="Wildfire Growth", description="Gain an empty Mana Crystal.")
+wildfire_growth_card = Card(cost=2, effect=wildfire_growth,
+                            name="Wildfire Growth", description="Gain an empty Mana Crystal.")
 
-starfire_card = Card(cost=6, effect=starfire, name="Starfire", description="Deal 5 damage. Draw a card.")
+starfire_card = Card(cost=6, effect=starfire, name="Starfire",
+                     description="Deal 5 damage. Draw a card.")
 
-bite_card = Card(cost=4, effect=bite, name="Bite", description="Give your hero 4 Attack this turn. Gain 4 armor.")
+bite_card = Card(cost=4, effect=bite, name="Bite",
+                 description="Give your hero 4 Attack this turn. Gain 4 armor.")
 
 # Neutrals
 # No card text
 
-wisp_card = Minion(cost=0, attack=1, health=1, effect=minion_no_effect, name="Wisp", description="")
+wisp_card = Minion(cost=0, attack=1, health=1,
+                   effect=minion_no_effect, name="Wisp", description="")
 
-murloc_raider_card = Minion(cost=1, attack=2, health=1, effect=minion_no_effect, name="Murloc Raider", description="")
+murloc_raider_card = Minion(cost=1, attack=2, health=1,
+                            effect=minion_no_effect, name="Murloc Raider", description="")
 
-bloodfen_raptor_card = Minion(cost=2, attack=3, health=2, effect=minion_no_effect, name="Bloodfen Raptor", description="")
+bloodfen_raptor_card = Minion(cost=2, attack=3, health=2,
+                              effect=minion_no_effect, name="Bloodfen Raptor", description="")
 
-river_crocolisk_card = Minion(cost=2, attack=2, health=3, effect=minion_no_effect, name="River Crocolisk", description="")
+river_crocolisk_card = Minion(cost=2, attack=2, health=3,
+                              effect=minion_no_effect, name="River Crocolisk", description="")
 
-magma_rager_card = Minion(cost=3, attack=5, health=1, effect=minion_no_effect, name="Magma Rager", description="")
+magma_rager_card = Minion(cost=3, attack=5, health=1,
+                          effect=minion_no_effect, name="Magma Rager", description="")
 
-chillwind_yeti_card = Minion(cost=4, attack=4, health=5, effect=minion_no_effect, name="Chillwind Yeti", description="")
+chillwind_yeti_card = Minion(cost=4, attack=4, health=5,
+                             effect=minion_no_effect, name="Chillwind Yeti", description="")
 
-oasis_snapjaw_card = Minion(cost=4, attack=2, health=7, effect=minion_no_effect, name="Oasis Snapjaw", description="")
+oasis_snapjaw_card = Minion(cost=4, attack=2, health=7,
+                            effect=minion_no_effect, name="Oasis Snapjaw", description="")
 
-boulderfirst_ogre = Minion(cost=6, attack=6, health=7, effect=minion_no_effect, name="Boulderfirst Ogre", description="")
+boulderfirst_ogre = Minion(cost=6, attack=6, health=7,
+                           effect=minion_no_effect, name="Boulderfirst Ogre", description="")
 
-core_hound_card = Minion(cost=7, attack=9, health=5, effect=minion_no_effect, name="Core Hound", description="")
+core_hound_card = Minion(cost=7, attack=9, health=5,
+                         effect=minion_no_effect, name="Core Hound", description="")
 
-war_golem_card = Minion(cost=7, attack=7, health=7, effect=minion_no_effect, name="War Golem", description="")
+war_golem_card = Minion(cost=7, attack=7, health=7,
+                        effect=minion_no_effect, name="War Golem", description="")
 
 # Battlecry
 
 # TODO, battlecry effect targeting
-abusive_sergeant_card = Minion(cost=1, attack=2, health=1, effect=abusive_sergeant, name="Abusive Sergeant", description="Battlecry: Give a minion +2 Attack this turn.") 
+abusive_sergeant_card = Minion(cost=1, attack=2, health=1, effect=abusive_sergeant,
+                               name="Abusive Sergeant", description="Battlecry: Give a minion +2 Attack this turn.")
 
-bloodsail_corsair_card = Minion(cost=1, attack=1, health=2, effect=bloodsail_corsair, name="Bloodsail Corsair", description="")
+bloodsail_corsair_card = Minion(
+    cost=1, attack=1, health=2, effect=bloodsail_corsair, name="Bloodsail Corsair", description="")
 
-elven_archer_card = Minion(cost=1, attack=1, health=1, effect=elven_archer, name="Elven Archer", description="Battlecry: Deal 1 damage.")
+elven_archer_card = Minion(cost=1, attack=1, health=1, effect=elven_archer,
+                           name="Elven Archer", description="Battlecry: Deal 1 damage.")
 
-hungry_crab_card = Minion(cost=1, attack=1, health=2, effect=hungry_crab, name="Hungry Crab", description="Battlecry: Destroy a Murloc and gain +2/+2.")
+hungry_crab_card = Minion(cost=1, attack=1, health=2, effect=hungry_crab,
+                          name="Hungry Crab", description="Battlecry: Destroy a Murloc and gain +2/+2.")
 
-voodoo_doctor_card = Minion(cost=1, attack=2, health=1, effect=voodoo_doctor, name="Voodoo Doctor", description="Battlecry: Restore 2 Health.")
+voodoo_doctor_card = Minion(cost=1, attack=2, health=1, effect=voodoo_doctor,
+                            name="Voodoo Doctor", description="Battlecry: Restore 2 Health.")
 
-acidic_swamp_ooze_card = Minion(cost=2, attack=3, health=2, effect=acidic_swamp_ooze, name="Acidic Swamp Ooze", description="Battlecry: Destroy your opponent's weapon.")
+acidic_swamp_ooze_card = Minion(cost=2, attack=3, health=2, effect=acidic_swamp_ooze,
+                                name="Acidic Swamp Ooze", description="Battlecry: Destroy your opponent's weapon.")
 
-bloodsail_raider_card = Minion(cost=2, attack=2, health=3, effect=elven_archer, name="Bloodsail Raider", description="Battlecry: Gain Attack equal to the Attack of your weapon.")
+bloodsail_raider_card = Minion(cost=2, attack=2, health=3, effect=elven_archer, name="Bloodsail Raider",
+                               description="Battlecry: Gain Attack equal to the Attack of your weapon.")
 
 
 cards = [
@@ -244,6 +282,7 @@ incomplete_cards = [
     hungry_crab_card,
     voodoo_doctor_card
 ]
+
 
 def get_random_deck():
     deck = []
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from cards import *
 from typing import List, Dict
 
+
 class Player:
-    def __init__(self, deck=None, renathal=False):
+    def __init__(self, deck=None, renathal=False, hero_class="Mage"):
         self.health = 35 if renathal else 30
         self.max_health = 35 if renathal else 30
+        self.hero_class = hero_class
+        self.hero_power = True
         self.armor = 0
         self.mana = 0
         self.max_mana = 0
@@ -13,10 +16,12 @@ class Player:
         self.attack = 0
         self.fatigue = 0
         self.hand: List[Card] = []
-        self.board: List[Card] = [] # minions and locations. array is good enough. O(1) time
+        # minions and locations. array is good enough. O(1) time
+        self.board: List[Card] = []
         self.played: Dict[Card] = {}
         self.destroyed: Dict[Card] = {}
-        self.deck: List[Card] = [moonfire_card for _ in range(30)] if deck is None else deck
+        self.deck: List[Card] = [
+            moonfire_card for _ in range(30)] if deck is None else deck
 
     def draw(self, amount=1):
         if self.deck:
@@ -35,9 +40,21 @@ class Player:
             return
 
         self.mana -= card.mana_cost
-        popped_card:Card = self.hand.pop(card_index)
+        popped_card: Card = self.hand.pop(card_index)
         popped_name = popped_card.name
         self.played[popped_name] = self.played.get(popped_name, 0) + 1
+
+    def use_hero_power(self):
+        # TODO
+        if self.hero_power:
+            if self.mana >= 2:
+                self.hero_power = False
+                print("Hero power used.")
+            else:
+                print("Not enough mana.")
+        else:
+            print("Hero power already used.")
+        return
 
 
 class Card:
@@ -45,19 +62,21 @@ class Card:
         self.name = name
         self.description = description
         self.mana_cost = cost
-        self.effect = effect # effect for minions would be battlecry (it needs to survive to activate though)
+        # effect for minions would be battlecry (it needs to survive to activate though)
+        self.effect = effect
 
     def play(self, player: Player, opponent: Player):
         player.played[self.name] = player.played.get(self.name, 0) + 1
         if self.effect(player, opponent) is not None:
             return False
-    
+
+
 class Minion(Card):
     def __init__(self, cost, attack, health, effect, name, description):
         super().__init__(cost, effect, name, description)
         self.attack = attack
         self.health = health
-    
+
     def play(self, player: Player, opponent: Player):
         super().play(player, opponent)
         # TODO: cannot play if boardsize is 7
@@ -65,7 +84,7 @@ class Minion(Card):
             player.board.append(self)
         else:
             print("Choose where to play the minion.")
-            n_possible = min(7, len(player.board) + 1 )
+            n_possible = min(7, len(player.board) + 1)
             board_copy: List[Card] = player.board
             for i in range(n_possible - 1):
                 print(i + 1, board_copy[i].name, end=" ")
@@ -77,6 +96,7 @@ class Minion(Card):
                 player.board.insert(pos - 1, self)
             except ValueError:
                 print("no bueno")
+
 
 class Game:
     def __init__(self, p1_deck=None, p2_deck=None):
@@ -97,13 +117,15 @@ class Game:
         while not game_end:
             self.turn(player=self.player1, index=1, opponent=self.player2)
 
-            game_end, winner = self.game_over(self.player1.health, self.player2.health)
+            game_end, winner = self.game_over(
+                self.player1.health, self.player2.health)
             if game_end:
                 return winner
 
             self.turn(player=self.player2, index=2, opponent=self.player1)
 
-            game_end, winner = self.game_over(self.player1.health, self.player2.health)
+            game_end, winner = self.game_over(
+                self.player1.health, self.player2.health)
             if game_end:
                 return winner
 
@@ -112,42 +134,43 @@ class Game:
         if player.max_mana < 10:
             player.max_mana += 1
         player.mana = min(10, player.max_mana)
+        # reset hero power
+        player.hero_power = True
         player.attack = 0
         player.draw()
 
         # printing for debugging/interface
         print(f"Player {index} mana: {player.mana}/{player.max_mana}")
-        print("Your hand:")
-        for i, card in enumerate(player.hand):
-            print(f"{i + 1}. {card.name}, {card.mana_cost} Mana. {card.description}")
+        self.print_hand(player)
         self.print_board()
         print(f"Player {index} HP: ", player.health)
         print(f"Player {1 if index == 2 else 2} HP: ", opponent.health)
-        
+
         while True:
             card_index = input()
+
             if not card_index:
                 break
             try:
                 card_index = int(card_index) - 1
 
                 # play cards instantly here rather than after all input
-                if card_index < len(player.hand):
+
+                if card_index == -1:
+                    player.use_hero_power()
+                elif 0 <= card_index < len(player.hand):
                     card: Card = player.hand[card_index]
                     if player.mana >= card.mana_cost:
-                        player.play(card=card, card_index=card_index, opponent=opponent)
+                        player.play(card=card, card_index=card_index,
+                                    opponent=opponent)
                         print(f"Current mana: {player.mana}/{player.max_mana}")
                         # print all cards again since card indices are different
-                        print("Your hand:")
-                        for i, card in enumerate(player.hand):
-                            print(f"{i + 1}. {card.name}, {card.mana_cost} MANA. {card.description}")
+                        self.print_hand(player)
                         self.print_board()
                     else:
                         print("Not enough mana.")
                 else:
                     print("Not a valid card index.")
-
-                
 
             except ValueError:
                 print(
@@ -155,8 +178,18 @@ class Game:
                 )
 
     def print_board(self):
-        print(f"Player1 board: {', '.join(card.name for card in self.player1.board)}")
-        print(f"Player2 board: {', '.join(card.name for card in self.player2.board)}")
+        print(
+            f"Player1 board: {', '.join(card.name for card in self.player1.board)}")
+        print(
+            f"Player2 board: {', '.join(card.name for card in self.player2.board)}")
+
+    def print_hand(self, player: Player):
+        print("Hand:")
+        print(f"0. Hero Power {int(player.hero_power)}/1")
+        for i, card in enumerate(player.hand):
+            print(
+                f"{i + 1}. {card.name}, {card.mana_cost} MANA. {card.description}")
+        pass
 
     def game_over(self, p1_health, p2_health):
         if p1_health < 1:
